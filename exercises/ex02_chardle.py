6999cd5f991e3ae3528266a1b15846638e605268
lukehkelly <luke.kelly0529@gmail.com>
2024-09-23 18:16:05 -0400

    finished ex02

--- exercises/ex02_chardle.py
+++ exercises/ex02_chardle.py
@@ -13,7 +13,7 @@ def input_word() -> str:
     return word
 
 
-def input_character() -> str:
+def input_letter() -> str:
     letter: str = input("Enter a single character: ")
 
     if len(letter) != 1:  # Checks if letter is a single character
@@ -25,23 +25,35 @@ def input_character() -> str:
 
 def contains_char(word: str, letter: str) -> None:
     print("Searching for " + letter + " in " + word)
-
+    count: int = 0  # count variable to
     # If statements to check each index for letter
     if word[0] == letter:
         print(letter + " found at index 0")
+        count += 1
     if word[1] == letter:
         print(letter + " found at index 1")
+        count += 1
     if word[2] == letter:
         print(letter + " found at index 2")
+        count += 1
     if word[3] == letter:
         print(letter + " found at index 3")
+        count += 1
     if word[4] == letter:
         print(letter + " found at index 4")
+        count += 1
+    # if-elif-else block to print correct instance count statement
+    if count == 0:
+        print("No instances of " + letter + " found in " + word)
+    elif count == 1:
+        print("1 instance of " + letter + " found in " + word)
+    else:
+        print(str(count) + " instances of " + letter + " found in " + word)
 
 
 def main() -> None:
     # Centralizes the code into one function
-    contains_char(word=input_word(), letter=input_character())
+    contains_char(word=input_word(), letter=input_letter())
 
 
 if __name__ == "__main__":
